93c7dd8aefaab0a54476bc922ac161b2796c5c41
BuildTools <unconfigured@null.spigotmc.org>
2021-05-22 19:41:32 +0100

    Auto deploy test

--- src/components/sections/HomeFront/HomeFront.tsx
+++ src/components/sections/HomeFront/HomeFront.tsx
@@ -70,7 +70,7 @@ function HomeFront(props: Props) {
               align="center"
               style={{ marginTop: theme.spacing(4) }}
             >
-              With 3 years of programming experience and more projects than I
+              with 3 years of programming experience and more projects than I
               can remember.
             </Typography>
             <Typography
