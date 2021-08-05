d05d3d55c94a0cd7d2b62caabdb30bb46011d6a3
Nathan <nathan@silverchip.com>
2021-08-05 11:42:49 +0100

    changed wording

--- src/components/pieces/projects/duct/ProjectDuct.tsx
+++ src/components/pieces/projects/duct/ProjectDuct.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import HomePanelProject from '../../../sections/HomePanel/HomePanelProject'
+import React from "react";
+import HomePanelProject from "../../../sections/HomePanel/HomePanelProject";
 
 import screenshot1 from "./media/screenshot1.png";
 import screenshot2 from "./media/screenshot2.png";
@@ -10,46 +10,68 @@ import screenshot5 from "./media/screenshot5.png";
 export default function ProjectDuct() {
   return (
     <HomePanelProject
-        title="Duct" 
-        subTitle="An Image Processing and PDF Generation Package (2020 - 2021)"
-        repoLink="https://github.com/IronicPickle/dw-piper"
-        imgs={[
-          { src: screenshot1, title: "Home Page" },
-          { src: screenshot2, title: "User Management Page" },
-          { src: screenshot3, title: "Profile Page" },
-          { src: screenshot4, title: "Create Server Menu" },
-          { src: screenshot5, title: "Create News Post Menu" },
-        ]}
-        about={
-          <>
-            Duct is an screen capture, image processing, PDF generation software package... I know, a handful...<br/>
-            Much like Search Companion, this package was also designed for use in my current workplace, where it's used to speed up the process of producing drainage reports.
-            It extracts coloured lines used by water companies to denote the location of water mains and sewers on a given map.
-            It allows the user to transplant those lines onto a map of their choosing, allowing the user to perform the relevant alignments.<br/>
-            <br/>
-            It works by capturing an image of the screen and isolating specific colour channels,
-            which are then applied as a mask to the whole image to produce a seires of individual images containing each line variant.
-            These images are than combined with the map supplied by the user, to generate a full image.
-            The end result is an image that contains the lines sourced from the water company's map combined with the map supplied by the user.
-            This image is placed into a template PDF which can be used to produce a full report.<br/>
-            <br/>
-            Also worth mentioning is that it is maintained via an&nbsp;
-            <a href="https://github.com/IronicPickle/update-server" >update server</a>
-            &nbsp;running Node.js, that allows the client to perform regular update checks.<br/>
-            <br/>
-            <b>What is was Written in</b><br/>
-            - Python<br/>
-            - Pascal (very little)<br/>
-            <br/>
-            <b>What is was Built with</b><br/>
-            - OpenCV<br/>
-            - Tkinter<br/>
-            - NumPy<br/>
-            - PyMuPDF<br/>
-            - PyInstaller<br/>
-            - Inno Setup
-          </>
-        }
-      />
-  )
+      title="Duct"
+      subTitle="An Image Processing and PDF Generation Package (2020 - 2021)"
+      repoLink="https://github.com/IronicPickle/dw-piper"
+      imgs={[
+        { src: screenshot1, title: "Home Page" },
+        { src: screenshot2, title: "User Management Page" },
+        { src: screenshot3, title: "Profile Page" },
+        { src: screenshot4, title: "Create Server Menu" },
+        { src: screenshot5, title: "Create News Post Menu" },
+      ]}
+      about={
+        <>
+          Duct is an screen capture, image processing, PDF generation software
+          package... I know, a handful...
+          <br />
+          Much like Search Companion, this package was also designed for use in
+          my previous workplace, where it was used to speed up the process of
+          producing drainage reports. It extracts coloured lines used by water
+          companies to denote the location of water mains and sewers on a given
+          map. It allows the user to transplant those lines onto a map of their
+          choosing, allowing the user to perform the relevant alignments.
+          <br />
+          <br />
+          It works by capturing an image of the screen and isolating specific
+          colour channels, which are then applied as a mask to the whole image
+          to produce a seires of individual images containing each line variant.
+          These images are than combined with the map supplied by the user, to
+          generate a full image. The end result is an image that contains the
+          lines sourced from the water company's map combined with the map
+          supplied by the user. This image is placed into a template PDF which
+          can be used to produce a full report.
+          <br />
+          <br />
+          Also worth mentioning is that it is maintained via an&nbsp;
+          <a href="https://github.com/IronicPickle/update-server">
+            update server
+          </a>
+          &nbsp;running Node.js, that allows the client to perform regular
+          update checks.
+          <br />
+          <br />
+          <b>What is was Written in</b>
+          <br />
+          - Python
+          <br />
+          - Pascal (very little)
+          <br />
+          <br />
+          <b>What is was Built with</b>
+          <br />
+          - OpenCV
+          <br />
+          - Tkinter
+          <br />
+          - NumPy
+          <br />
+          - PyMuPDF
+          <br />
+          - PyInstaller
+          <br />- Inno Setup
+        </>
+      }
+    />
+  );
 }
--- src/components/pieces/projects/search_companion/ProjectSearchCompanion.tsx
+++ src/components/pieces/projects/search_companion/ProjectSearchCompanion.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import HomePanelProject from '../../../sections/HomePanel/HomePanelProject'
+import React from "react";
+import HomePanelProject from "../../../sections/HomePanel/HomePanelProject";
 
 import screenshot1 from "./media/screenshot1.png";
 import screenshot2 from "./media/screenshot2.png";
@@ -10,42 +10,63 @@ import screenshot5 from "./media/screenshot5.png";
 export default function ProjectSearchCompanion() {
   return (
     <HomePanelProject
-        title="Search Companion" 
-        subTitle="A Data Extraction and Injection Browser Extension (2021)"
-        repoLink="https://github.com/IronicPickle/search-companion"
-        imgs={[
-          { src: screenshot1, title: "Menu and Tabs" },
-          { src: screenshot2, title: "Dark Mode Menu and Tabs" },
-          { src: screenshot3, title: "Property Info Tab" },
-          { src: screenshot4, title: "Product Info Tab" },
-          { src: screenshot5, title: "Mapping Tab" },
-        ]}
-        about={
-          <>
-            This project was designed from the ground up for use in my current workplace.
-            It's main use case is the extraction of data from the Case Management System (CMS) used in-house.
-            It maintains a temporary store of data that relates to the case the user is currently accessing via our CMS.
-            This data becomes accessible from all browser windows and tabs, speeding up various processes.
-            It can also extract and inject information into various other relevant web pages to speed up the ordering of products and the production of reports.<br/>
-            <br/>
-            I maintain this as the sole developer on behalf of the company, where it is used by most members of the office on a daily basis.
-            It is currently available on both the Chrome Web Store and Firefox Add-ons Store, though it is either unlisted or listed with low priority, as the extension is not intended for public use.
-            The extension can be found via the following links, though please note, it is largely unusable without access to the CMS it was designed for.<br/>
-            <br/>
-            <a href="https://chrome.google.com/webstore/detail/search-companion/kdmjohdcocbbjfelihflkakgimkefabo" >Chrome</a> and&nbsp;
-            <a href="https://addons.mozilla.org/en-GB/firefox/addon/search-companion/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search" >Firefox</a><br/>
-            <br/>
-            <b>What is was Written in</b><br/>
-            - TypeScript<br/>
-            - HTML<br/>
-            - CSS<br/>
-            <br/>
-            <b>What is was Built with</b><br/>
-            - React.js<br/>
-            - Material-UI<br/>
-            - Webpack
-          </>
-        }
-      />
-  )
+      title="Search Companion"
+      subTitle="A Data Extraction and Injection Browser Extension (2021)"
+      repoLink="https://github.com/IronicPickle/search-companion"
+      imgs={[
+        { src: screenshot1, title: "Menu and Tabs" },
+        { src: screenshot2, title: "Dark Mode Menu and Tabs" },
+        { src: screenshot3, title: "Property Info Tab" },
+        { src: screenshot4, title: "Product Info Tab" },
+        { src: screenshot5, title: "Mapping Tab" },
+      ]}
+      about={
+        <>
+          This project was designed from the ground up for use in my previous
+          workplace. It's main use case is the extraction of data from the Case
+          Management System (CMS) used in-house. It maintains a temporary store
+          of data that relates to the case the user is currently accessing via
+          the CMS. This data becomes accessible from all browser windows and
+          tabs, speeding up various processes. It can also extract and inject
+          information into various other relevant web pages to speed up the
+          ordering of products and the production of reports.
+          <br />
+          <br />
+          I maintained this as the sole developer on behalf of the company,
+          where it was used by most members of the office on a daily basis. It
+          is currently available on both the Chrome Web Store and Firefox
+          Add-ons Store, though it is either unlisted or listed with low
+          priority, as the extension is not intended for public use. The
+          extension can be found via the following links, though please note, it
+          is largely unusable without access to the CMS it was designed for.
+          <br />
+          <br />
+          <a href="https://chrome.google.com/webstore/detail/search-companion/kdmjohdcocbbjfelihflkakgimkefabo">
+            Chrome
+          </a>{" "}
+          and&nbsp;
+          <a href="https://addons.mozilla.org/en-GB/firefox/addon/search-companion/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search">
+            Firefox
+          </a>
+          <br />
+          <br />
+          <b>What is was Written in</b>
+          <br />
+          - TypeScript
+          <br />
+          - HTML
+          <br />
+          - CSS
+          <br />
+          <br />
+          <b>What is was Built with</b>
+          <br />
+          - React.js
+          <br />
+          - Material-UI
+          <br />- Webpack
+        </>
+      }
+    />
+  );
 }
