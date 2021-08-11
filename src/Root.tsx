a53417a34e73cfc6834615fa85b82b213e1e522c
Nathan <nathan@silverchip.com>
2021-08-11 22:24:25 +0100

    added react google analytics

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,14 +1,18 @@
-import { ThemeProvider } from '@material-ui/styles'
-import React from 'react'
-import Header from './components/Header';
-import GlobalCSS from './GlobalCSS';
-import Home from './pages/Home'
-import mainTheme from './themes/mainTheme'
+import { ThemeProvider } from "@material-ui/styles";
+import React from "react";
+import Header from "./components/Header";
+import GlobalCSS from "./GlobalCSS";
+import Home from "./pages/Home";
+import mainTheme from "./themes/mainTheme";
+import ReactGA from "react-ga";
 
 const theme = mainTheme;
 
 export default function Root() {
-  document.body.style.backgroundColor = theme.palette.background.default
+  document.body.style.backgroundColor = theme.palette.background.default;
+
+  ReactGA.initialize("G-W5GKTMY7C5");
+  ReactGA.pageview(window.location.pathname + window.location.search);
 
   return (
     <ThemeProvider theme={theme}>
@@ -16,5 +20,5 @@ export default function Root() {
       <Header />
       <Home />
     </ThemeProvider>
-  )
+  );
 }
